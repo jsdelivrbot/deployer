2515fb226400a77f0ab1af7ae83cf5204ad2a668
nyashanu <lankienyashanu@gmail.com>
2017-08-16 05:10:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+const todoController = require('./Controllers/todoController');
+todoController(app)
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
