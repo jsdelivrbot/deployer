7ee271a5190eb202319f89d4102f30259e6fc7fb
nyashanu <lankienyashanu@gmail.com>
2017-08-16 05:25:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/signup', function (req,res) {
-  res.render('signup'); /*todo*/
-});
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -19,4 +15,21 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+
+app.get('/', function (req,res) {
+    res.render('index'); /*todo*/
+  });
+
+  app.get('/signup', function (req,res) {
+      res.render('signup'); /*todo*/
+    });
+
+app.get('/nav', function (req,res) {
+  res.render('skin'); /*todo*/
+  // get data from moongo and pass it to view
+  // Todo.find({}, function (err,data) {
+  //   if(err) throw err;
+  // });
 });
