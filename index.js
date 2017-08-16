3affb3a6c48de596f51c726dd21774190fd28a2c
nyashanu <lankienyashanu@gmail.com>
2017-08-16 17:11:47 +0200

    Public  fix

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render(#');
 });
+
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
